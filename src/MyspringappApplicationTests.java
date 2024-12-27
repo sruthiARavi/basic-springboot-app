0bab311a66b9f5caad18a9ff0b11d2c5479d1bce
Sruthi Ravichandran <166052788+sruthiARavi@users.noreply.github.com>
2024-12-27 01:15:25 -0500

    Add files via upload

---
+++ src/MyspringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.demo.myspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
